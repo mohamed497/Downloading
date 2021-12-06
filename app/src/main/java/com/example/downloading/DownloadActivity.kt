ed37bdf441c51b5fe4b0f2cff009f909e3f936ed
moham <mohamedgamaal20144@gmail.com>
2021-12-06 18:35:01 +0200

    downloading 1 file

---
+++ app/src/main/java/com/example/downloading/DownloadActivity.kt
@@ -0,0 +1,116 @@
+package com.example.downloading
+
+import android.Manifest
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.work.*
+import com.example.downloading.base.GlobalConstants.PERMISION_REQUEST
+import com.example.downloading.base.RunTimePermission
+import com.example.downloading.service.DownloadService
+import kotlinx.android.synthetic.main.activity_download.*
+import java.util.concurrent.TimeUnit
+
+class DownloadActivity : AppCompatActivity() {
+    private var runtimePermission: RunTimePermission = RunTimePermission(this)
+    private val workManager = WorkManager.getInstance(this)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_download)
+
+
+        btnStartDownloadWork.setOnClickListener { view ->
+            when (view.id) {
+                R.id.btnStartDownloadWork -> {
+                    runtimePermission.requestPermission(listOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                        object : RunTimePermission.PermissionCallback {
+                            override fun onGranted() {
+
+                                StartOneTimeWorkManager()
+                            }
+
+                            override fun onDenied() {
+                                Toast.makeText(
+                                    this@DownloadActivity,
+                                    "Need Permission of Storage",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                        })
+                }
+
+            }
+        }
+    }
+
+    private fun StartOneTimeWorkManager() {
+
+        val constraints =
+            Constraints.Builder().setRequiredNetworkType(NetworkType.CONNECTED).build()
+        val task =
+            OneTimeWorkRequest.Builder(DownloadWorker::class.java).setConstraints(constraints)
+                .setBackoffCriteria(
+                    BackoffPolicy.LINEAR,
+                    OneTimeWorkRequest.DEFAULT_BACKOFF_DELAY_MILLIS,
+                    TimeUnit.MILLISECONDS
+                )
+                .build()
+
+        workManager.enqueue(task)
+        workManager.getWorkInfoByIdLiveData(task.id)
+            .observe(this@DownloadActivity, Observer { work ->
+
+                work?.let {
+                    if (work.state == WorkInfo.State.RUNNING) {
+                        Log.d(DownloadActivity::javaClass.name, "RUNNING")
+                        loaderShow(true)
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                            startForegroundService(Intent(
+                                applicationContext,
+                                DownloadService::class.java
+                            ))
+                        }
+
+                    } else
+                        if (work.state.isFinished) {
+                            Log.d(DownloadActivity::javaClass.name, "DONE")
+                            Toast.makeText(
+                                this@DownloadActivity,
+                                getString(R.string.work_done),
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            loaderShow(false)
+                        }
+                }
+            })
+    }
+
+
+    private fun loaderShow(flag: Boolean) {
+        when (flag) {
+            true -> llProgress.visibility = View.VISIBLE
+            false -> llProgress.visibility = View.GONE
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == PERMISION_REQUEST)
+            runtimePermission.onRequestPermissionsResult(grantResults)
+
+    }
+
+    override fun onPause() {
+        stopService(Intent(this, DownloadService::class.java))
+        super.onPause()
+    }
+}
---
+++ app/src/main/java/com/example/downloading/base/GlobalConstants.kt
@@ -0,0 +1,15 @@
+package com.example.downloading.base
+
+object GlobalConstants {
+
+    const val PERMISION_REQUEST = 100
+    const val PERIODIC_INTERVAL = 15L
+    const val CHANNEL_ID = "ChannelId1"
+
+    //    const val URLFILE = "<YOUR_FILE_URI>"
+        const val URLFILE = "https://clx.fcbddaebbddbaebaddd.xyz/?file=M3R4SUNiN3JsOHJ6WWQ2a3NQS1Y5ZGlxVlZIOCtyaDEwOFkveEZzVktJOXBpYkk1aHJEMEU0ZENQdk5KeTRPdUZPRkU5Q3JRTEpYWkZoYUdvNHd6UUhiRXBmQXdxV3VLb2FoMEhKdFhRaFQydC9lUm9XaFRqMWZLYUltWlFZZ1lZR2N2NVVkbTNIdTI2N2ZSb1FYeG9YaTVya0NHTVhWYzRXTmRhNkdJb013T3htRFJhcXEyaDhOVzZ6V0p2NTVMaTZBPQ%3D%3D"
+//    const val URLFILE = "https://static0.gamerantimages.com/wordpress/wp-content/uploads/2021/11/Arcane-Vi-1.jpg"
+//    const val URLFILE = "https://www.youtube.com/watch?v=s_C0CDkP3Cc&ab_channel=Wegz%D9%88%D9%8A%D8%AC%D8%B2"
+
+
+}
